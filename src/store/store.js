355f2cf8c6aab3ff66637fbbaa6d60294a6de7f9
XiaoFei <103562083+xiaofei120615@users.noreply.github.com>
2022-04-23 14:11:39 +0800

    Add files via upload

--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,55 @@
+import Vue from "vue";
+import vuex from 'vuex';
+Vue.use(vuex)
 
+//切换昼夜
+const tabActive = {
+    namespaced: true,
+    state: {
+        //false代表白天，true代表黑夜
+        active: false
+    },
+    actions: {
+
+    },
+    mutations: {
+        updateActive(state){
+            state.active = !state.active
+        }
+    },
+    getters: {
+        getActive(state){
+            return state.active
+        }
+    }
+}
+
+//扫雷最好成绩
+const sweeperRank = {
+    namespaced : true,
+    state: {
+        rank: {
+            name: '',
+            bombs: 0,
+            time: 0
+        }
+    },
+    mutations: {
+        updateRank(state, value) {
+            console.log(value);
+            state.rank = value
+        }
+    },
+    getters: {
+        getRank(state) {
+            return state.rank
+        }
+    }
+}
+
+export default new vuex.Store({
+    modules: {
+        tabActive,
+        sweeperRank
+    }
+})
